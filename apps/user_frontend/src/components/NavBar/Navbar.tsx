d12b8f6a188655e943e4e52286e48566599de045
saicharanbm <saicharanbm@gmail.com>
2024-12-31 02:05:43 +0530

    Add Navbar and UserModal components; implement user logout functionality with toast notifications and dynamic dropdown

---
+++ apps/user_frontend/src/components/NavBar/Navbar.tsx
@@ -0,0 +1,101 @@
+import { NavLink, useNavigate } from "react-router-dom";
+import { useRef, useState, useEffect } from "react";
+import UserModal from "./UserModal";
+
+type NavbarProps = {
+  userData?: { avatarUrl: string; fullName: string };
+};
+
+const Navbar = ({ userData }: NavbarProps) => {
+  const defaultAvatar = useRef(
+    "https://m.media-amazon.com/images/G/02/CerberusPrimeVideo-FN38FSBD/adult-2.png"
+  );
+
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const navigate = useNavigate();
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+
+  const handleMouseEnter = () => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current); // Clear any existing timer
+    }
+    setIsDropdownOpen(true); // Open dropdown
+  };
+
+  const handleMouseLeave = () => {
+    timeoutRef.current = setTimeout(() => {
+      setIsDropdownOpen(false); // Close dropdown after delay
+    }, 300); // Adjust the delay time as needed
+  };
+
+  // Close dropdown when userData changes (e.g., user logs out)
+  useEffect(() => {
+    if (!userData) {
+      setIsDropdownOpen(false);
+    }
+  }, [userData]);
+
+  // Clean up timeoutRef on unmount
+  useEffect(() => {
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, []);
+
+  return (
+    <nav className="h-16 bg-[#00050D] fixed top-0 w-full text-white flex items-center justify-between px-[6%] border-b-2 border-[#262d38] z-40">
+      <div className="flex space-x-12">
+        <div className="icon">
+          <h1
+            className="text-3xl font-bold tracking-wider text-[#fff] cursor-pointer"
+            onClick={() => navigate("/")}
+          >
+            <span>Skill</span>
+            <span className=" text-[#F89A28] font-helvetica">hub</span>
+          </h1>
+        </div>
+      </div>
+      {userData ? (
+        <div
+          className="relative"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <div
+            className={`rounded-full p-1 cursor-pointer ${
+              isDropdownOpen && "bg-white"
+            } transform duration-200 ease-in-out`}
+          >
+            <img
+              src={userData?.avatarUrl || defaultAvatar.current}
+              alt="profile picture"
+              className="w-10 h-10 rounded-full"
+            />
+          </div>
+          {isDropdownOpen && <UserModal fullName={userData.fullName} />}
+        </div>
+      ) : (
+        <div className="flex items-center space-x-4">
+          {["/login", "/signup"].map((authPath, index) => (
+            <NavLink
+              key={index}
+              to={authPath}
+              className={({ isActive }) =>
+                `text-lg p-1 rounded cursor-pointer hover:bg-[#F89A28] hover:opacity-100 hover:font-semibold hover:text-gray-800 ${
+                  isActive &&
+                  "bg-[#F89A28] text-gray-800 opacity-50 font-semibold"
+                }`
+              }
+            >
+              {authPath === "/login" ? "Login" : "Signup"}
+            </NavLink>
+          ))}
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ apps/user_frontend/src/components/NavBar/UserModal.tsx
@@ -0,0 +1,82 @@
+import { MdOutlineLogout } from "react-icons/md";
+import { IoMdCreate, IoMdHome } from "react-icons/io";
+import { IoSettings } from "react-icons/io5";
+import { NavLink } from "react-router-dom";
+import { useLogoutMutation } from "../../services/mutations";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
+function UserModal({ fullName }: { fullName: string }) {
+  const navigate = useNavigate();
+  const { mutateAsync: logout } = useLogoutMutation();
+  const handleLogout = () => {
+    toast.promise(logout(), {
+      pending: "Logging out...",
+      success: {
+        render() {
+          navigate("/login");
+          return "Logout successful!";
+        },
+      },
+      error: {
+        render({ data }: { data: string }) {
+          console.log(data);
+          return (data as string) || "Logout failed!";
+        },
+      },
+    });
+  };
+
+  return (
+    <div className="absolute right-0 my-4 w-64 bg-[rgba(25,30,37,.95)] rounded-md shadow-lg text-white">
+      <div className="px-4 py-2 border-b border-gray-600 text-sm">
+        <p className="text-lg text-white">
+          Hello, <span className="font-bold text-[#F89A28]">{fullName}</span>
+        </p>
+      </div>
+      <div className=" flex flex-col space-y-2 text-sm:text-lg">
+        <NavLink
+          to={"/"}
+          className={({ isActive }) =>
+            `px-4 py-2 hover:text-[#F89A28] cursor-pointer flex space-x-2 items-center ${
+              isActive && "bg-gray-700 text-[#F89A28] font-semibold"
+            }`
+          }
+        >
+          <IoMdHome className="text-xl" /> <p>Home</p>
+        </NavLink>
+        <NavLink
+          to={"/create-course"}
+          className={({ isActive }) =>
+            `px-4 py-2 hover:text-[#F89A28] cursor-pointer flex space-x-2 items-center ${
+              isActive && "bg-gray-700 text-[#F89A28] font-semibold"
+            }`
+          }
+        >
+          <IoMdCreate /> <p>Create Course</p>
+        </NavLink>
+        <NavLink
+          to={"/account-setting"}
+          className={({ isActive }) =>
+            `px-4 py-2 hover:text-[#F89A28] cursor-pointer flex space-x-2 items-center ${
+              isActive && "bg-gray-700 text-[#F89A28] font-semibold"
+            }`
+          }
+        >
+          <IoSettings /> <p>Account & Settings</p>
+        </NavLink>
+      </div>
+
+      <div className="px-4 py-2 border-t border-gray-600">
+        <button
+          onClick={handleLogout}
+          className="flex space-x-2 items-center hover:text-[#F89A28] cursor-pointer"
+        >
+          <MdOutlineLogout /> <p>Sign out</p>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default UserModal;
